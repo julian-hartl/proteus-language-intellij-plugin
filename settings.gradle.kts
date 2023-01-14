a10768438d488ba490e3bb746a15e4cb2ba4c127
GitHub Action <action@github.com>
2023-01-14 17:10:21 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "proteus-language-intellij-plugin"
--- src/main/kotlin/com/github/julianhartl/proteuslanguageintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/julianhartl/proteuslanguageintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.julianhartl.proteuslanguageintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.julianhartl.proteuslanguageintellijplugin.MyBundle
 
 class MyApplicationService {
 
